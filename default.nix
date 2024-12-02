cb15f4526ce4635e96e6e49956f71d26b0b0cfbe
wrvsrx <wrvsrx@outlook.com>
2024-12-02 14:08:10 +0800

    Reduce time cost of west2nixHook

--- default.nix
+++ default.nix
@@ -1,8 +1,5 @@
 { callPackage, python3 }:
-rec {
+{
   west2nix = python3.pkgs.callPackage ./package.nix { };
   mkWest2nixHook = callPackage ./hook.nix { };
-  mkWestDependencies = callPackage ./make-west-dependencies.nix {
-    inherit mkWest2nixHook;
-  };
 }
