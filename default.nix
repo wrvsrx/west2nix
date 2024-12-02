ac7b39c7176f9b1dd22c64083c84a6f22ade30d2
wrvsrx <wrvsrx@outlook.com>
2024-12-02 09:43:31 +0800

    add `mkWestDependencies`

--- default.nix
+++ default.nix
@@ -1,5 +1,8 @@
 { callPackage, python3 }:
-{
+rec {
   west2nix = python3.pkgs.callPackage ./package.nix { };
   mkWest2nixHook = callPackage ./hook.nix { };
+  mkWestDependencies = callPackage ./make-west-dependencies.nix {
+    inherit mkWest2nixHook;
+  };
 }
